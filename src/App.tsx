ee9994174bf1db66b7b1109e8d116918db392451
divinevibration <83971946+divinevibration@users.noreply.github.com>
2022-01-07 22:16:52 +0200

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //Wallet connect function
+  const connectWallet = () => {};
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
+        <p>Hello Vite + React + Typescript!</p>
+
+        <button
+          className="flex flex-row justify-center items-center my-5 bg-[#0070F3] p-3 rounded-full cursor-pointer hover:bg-[#055ACA]"
+          onClick={connectWallet}
+        >
+          <p className="text-white text-base font-semibold">Connect Wallet</p>
+        </button>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
@@ -27,7 +30,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +42,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
